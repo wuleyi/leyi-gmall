1a7a98380cc844a0fc1e14152470c41c39371e89
wuleyi <965926593@qq.com>
2019-06-08 11:35:20 +0800

    gmall-parent 01.

---
+++ gmall-admin-web/src/main/java/org/devil/gmall/admin/GmallAdminWebApplication.java
@@ -0,0 +1,13 @@
+package org.devil.gmall.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmallAdminWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmallAdminWebApplication.class, args);
+    }
+
+}
